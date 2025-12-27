fffbef0101ec8da6ea7c06b6b345ad659f413488
Rigen Maulana <63197023+rigenski@users.noreply.github.com>
2025-12-27 21:02:16 +0700

    fix: remove pf

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -60,7 +60,7 @@ export default function Header() {
               animate="visible"
               transition={{ delay: 0.6 }}
             >
-              <Link
+              {/* <Link
                 href={process.env.NEXT_PUBLIC_PF_URL || "/"}
                 target="_blank"
               >
@@ -83,7 +83,7 @@ export default function Header() {
                     />
                   </div>
                 </motion.button>
-              </Link>
+              </Link> */}
               <Link href={process.env.NEXT_PUBLIC_X_URL || "/"} target="_blank">
                 <motion.button
                   className="min-h-full bg-[url('/components/header/btn-bg.png')] bg-cover bg-center px-4 py-1.5 md:px-8 md:py-2"
